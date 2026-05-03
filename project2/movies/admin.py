63934bb90de065f23073b192d3f9413d232c6902
roberttbdux <147278549+roberttbdux@users.noreply.github.com>
2026-05-03 08:24:04 +0000

    Add movie database models and admin registration

--- project2/movies/admin.py
+++ project2/movies/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import (
+    Slider,
+    Advertisement,
+    SocialLink,
+    Trailer,
+    TrailerItem,
+    Celebrity,
+    News,
+    Tweet,
+    MovieTheater,
+    MovieTV,
+    Newsletter,
+)
 
-# Register your models here.
+admin.site.register(Slider)
+admin.site.register(Advertisement)
+admin.site.register(SocialLink)
+admin.site.register(Trailer)
+admin.site.register(TrailerItem)
+admin.site.register(Celebrity)
+admin.site.register(News)
+admin.site.register(Tweet)
+admin.site.register(MovieTheater)
+admin.site.register(MovieTV)
+admin.site.register(Newsletter)
